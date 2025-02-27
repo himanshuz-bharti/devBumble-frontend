1ae1683373c1e11d95ab3b00a8f0bdb096373ad1
Himanshu <himbharti9902@gmail.com>
2025-02-27 19:20:18 +0530

    DasiyUi installion and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
 function App() {
-
   return (
     <>
-     <h1>Hi bhai</h1>
+      <div className="navbar bg-base-300 px-4">
+        <div className="flex flex-row justify-between w-full items-center">
+          {/* Left Side: DevTinder */}
+          <a className="btn btn-ghost text-xl font-bold">DevTinder</a>
+
+          {/* Right Side: Avatar */}
+          <div className="avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
